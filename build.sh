060555c1c54d6212efbf2df91a70f75f232840b2
Muthu ramalingam <128946991+Muthuinc@users.noreply.github.com>
2023-08-01 20:57:14 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-docker build -t muthuinc/react:v1 .
+docker build -t muthuinc/react:v2 .
 
 docker login -u muthuinc -p 8973214268
 
-docker push muthuinc/react:v1
+docker push muthuinc/react:v2
 
 echo "done"
